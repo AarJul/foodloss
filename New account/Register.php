18a2245be82e9ea83780a5b9a22568845b437542
QuanNguyen8701 <133239744+QuanNguyen8701@users.noreply.github.com>
2023-06-05 11:16:11 +0900

    Update Register.php

--- New account/Register.php
+++ New account/Register.php
@@ -12,22 +12,6 @@ if ($conn->connect_error) {
     die("アクセス失敗: " . $conn->connect_error);
 }
 
-//同じIDがすでに登録されたかどうかをチェックするメソッド
-function isDuplicateID($conn, $id) {
-    $sql = "SELECT COUNT(*) as count FROM your_table WHERE ID = '$id'";
-    $result = $conn->query($sql);
-    $row = $result->fetch_assoc();
-    return $row['count'] > 0;
-}
-
-//６桁のIDを乱数で割り当てる
-$id = mt_rand(100000, 999999);
-
-//重複がなくなるまでランダムな数値を確認して再生成する
-while (isDuplicateID($conn, $randomNumber)) {
-    $id = mt_rand(100000, 999999);
-}
-
 //フォームから送信されたデータを取得
 $name = $_POST['name'];
 $telephone = $_POST['telephone'];
@@ -37,7 +21,7 @@ $address = $_POST['address'];
 
 //SQL文を作成
 $sql = "INSERT INTO USER (name, telephone, email, password, address) 
-        VALUES ('$id','$name', '$telephone', '$email', '$password', '$address')";
+        VALUES ('$name', '$telephone', '$email', '$password', '$address')";
 
 //トランザクション開始
 if ($conn->query($sql) === TRUE) {
