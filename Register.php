ec9a9a7a7d0f4d8f2647fe90094ed1e34dd62a88
QuanNguyen8701 <quanrengar@gmail.com>
2023-06-06 11:39:38 +0900

    Register.php OK

--- Register.php
+++ Register.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 // データベースの情報　
 $servername = "localhost";
 $username = "your_username";
@@ -12,25 +12,35 @@ if ($conn->connect_error) {
     die("アクセス失敗: " . $conn->connect_error);
 }
 
+$sql;
+
 //フォームから送信されたデータを取得
 $name = $_POST['name'];
 $telephone = $_POST['telephone'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 $address = $_POST['address'];
+$selectedOption = $_POST['selectedOption'];
 
-//SQL文を作成
-$sql = "INSERT INTO USER (USER_NAME, USER_TEL, USER_EMAIL, USER_PASSWORD, USER_ADDRESS) 
-        VALUES ('$name', '$telephone', '$email', '$password', '$address')";
-
-//トランザクション開始
-if ($conn->query($sql) === TRUE) {
-    echo "登録成功";
-} else {
-    echo "エラー: " . $sql . "<br>" . $conn->error;
+if ($selectedOption == 'store') {
+    $stmt = $conn->prepare("INSERT INTO Store (STORE_NAME, STORE_TEL, STORE_EMAIL, STORE_PASSWORD, STORE_ADDRESS) 
+                            VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $name, $telephone, $email, $hashedPassword, $address);
+}else{
+    $stmt = $conn->prepare("INSERT INTO User (USER_NAME, USER_TEL, USER_EMAIL, USER_PASSWORD, USER_ADDRESS) 
+                            VALUES (?, ?, ?, ?, ?)");
+    $stmt->bind_param("sssss", $name, $telephone, $email, $hashedPassword, $address);
 }
 
-//トランザクション終了
+if ($stmt->execute()) {
+    echo "登録できた！";
+} else {
+    echo "Lỗi: " . $stmt->error;
+}
+
+$stmt->close();
+
 $conn->close();
 ?>
 
