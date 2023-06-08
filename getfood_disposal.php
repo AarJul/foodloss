9768dda96ebbf754cf4ff3647387b17caf2f6e9a
QuanNguyen8701 <quanrengar@gmail.com>
2023-06-08 19:17:36 +0900

    getfood_disposal.php updated

--- getfood_disposal.php
+++ getfood_disposal.php
@@ -1,9 +1,9 @@
 <?php
 // データベースの情報　
 $servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "your_database_name";
+$username = "dbuser";
+$password = "ecc";
+$dbname = "food";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -14,16 +14,54 @@ if ($conn->connect_error) {
 
 $sql;
 //フォームから送信されたデータを取得
-$email = $_POST['email'];
-$sql = "SELECT * FROM disposal WHERE store_id = (SELECT store_id FROM store WHERE store_email = '$email')";
+//$email = $_POST['email'];
+//$sql = "SELECT * FROM disposal WHERE store_id = (SELECT store_id FROM store WHERE store_email = '$email')";
+$sql = "SELECT * FROM disposal WHERE store_id = 12345";
+$result = $conn->query($sql);
 
-if ($stmt->execute()) {
-    echo "登録できた！";
-} else {
-    echo "エラー: " . $stmt->error;
+// 結果を配列に格納
+$rows = array();
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $rows[] = $row;
+    }
 }
-
-$stmt->close();
 
 $conn->close();
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>廃棄状況</title>
+</head>
+<body>
+    <h1>廃棄状況</h1>
+
+    <?php if (!empty($rows)) : ?>
+        <table>
+            <tr>
+                <th>Disposal ID</th>
+                <th>Store ID</th>
+                <th>Item</th>
+                <th>Qty</th>
+                <th>Date</th>
+                <th>Status</th>
+            </tr>
+            <?php foreach ($rows as $row) : ?>
+                <tr>
+                    <td><?php echo $row['DISPOSAL_ID']; ?></td>
+                    <td><?php echo $row['STORE_ID']; ?></td>
+                    <td><?php echo $row['ITEM']; ?></td>
+                    <td><?php echo $row['QTY']; ?></td>
+                    <td><?php echo $row['DATE']; ?></td>
+                    <td><?php echo $row['STATUS']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php else : ?>
+        <p>廃棄がなさそうですね！</p>
+    <?php endif; ?>
+
+</body>
+</html>
