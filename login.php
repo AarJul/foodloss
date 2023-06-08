9f93ed8f2f4ef3c827914b7fbb86a22f7195a2c7
mintie98 <thang3dinhat@gmail.com>
2023-06-09 00:49:15 +0900

    update1

--- login.php
+++ login.php
@@ -1,73 +1,72 @@
 <?php
 session_start();
-require_once dirname(__FILE__) . '/../function/auto_login.php';
-require_once dirname(__FILE__) . '/../function/db_connection.php';
+require_once dirname(__FILE__) . '/function/auto_login.php';
+require_once dirname(__FILE__) . '/function/db_connection.php';
 
 // Connect to the database
-$db = connection();
+$conn = connection();
 
-// Function to handle user login
-function user_login($Email, $password)
+// Function to check login and redirect users
+function login()
 {
-  $db = connection();
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
 
-  // Check login credentials in the store table
-  $store_query = "SELECT * FROM store WHERE STORE_EMAIL = :Email";
-  $store_stmt = $db->prepare($store_query);
-  $store_stmt->bindParam(':Email', $Email);
-  $store_stmt->execute();
+        if (!empty($email) && !empty($password)) {
+            $conn = connection();
 
-  // Check login with store information
-  if ($store_stmt->rowCount() > 0) {
-    $store = $store_stmt->fetch();
+            // Query the store table
+            $storeQuery = "SELECT * FROM store WHERE STORE_EMAIL = :email";
+            $storeStmt = $conn->prepare($storeQuery);
+            $storeStmt->bindParam(':email', $email);
+            $storeStmt->execute();
+            $storeResult = $storeStmt->fetch(PDO::FETCH_ASSOC);
 
-    // Verify password
-    if (password_verify($password, $store['STORE_PASSWORD'])) {
-      // Successful login with store account
-      // Save information to session or perform other actions
-      // Redirect to the store page
-      header("Location: store.php");
-      exit;
-    } else {
-      echo "Incorrect password!";
-      return;
+            // Query the user table
+            $userQuery = "SELECT * FROM user WHERE USER_EMAIL = :email";
+            $userStmt = $conn->prepare($userQuery);
+            $userStmt->bindParam(':email', $email);
+            $userStmt->execute();
+            $userResult = $userStmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($storeResult) {
+                // User with email exists in the store table
+                // Check the password
+                if (count($storeResult) > 0 && password_verify($password, $storeResult['STORE_PASSWORD'])) {
+                    // Successful login with store account
+                    // Save information to session or perform other actions
+                    $_SESSION['user_id'] = $storeResult['STORE_ID'];
+                    header("Location: store.php");
+                    exit();
+                } else {
+                    echo "Incorrect password!";
+                }
+            } elseif ($userResult) {
+                // User with email exists in the user table
+                // Check the password
+                if (count($userResult) > 0 && password_verify($password, $userResult['USER_PASSWORD'])) {
+                    // Successful login with user account
+                    // Save information to session or perform other actions
+                    $_SESSION['user_id'] = $userResult['USER_ID'];
+                    header("Location: user.php");
+                    exit();
+                } else {
+                    echo "Incorrect password!";
+                }
+            } else {
+                echo "User does not exist!";
+            }
+
+            $conn = null;
+        } else {
+            echo "Please enter email and password!";
+        }
     }
-  }
-
-  // Check login credentials in the user table
-  $user_query = "SELECT * FROM user WHERE USER_EMAIL = :Email";
-  $user_stmt = $db->prepare($user_query);
-  $user_stmt->bindParam(':Email', $Email);
-  $user_stmt->execute();
-
-  // Check login with user information
-  if ($user_stmt->rowCount() > 0) {
-    $user = $user_stmt->fetch();
-
-    // Verify password
-    if (password_verify($password, $user['USER_PASSWORD'])) {
-      // Successful login with user account
-      // Save information to session or perform other actions
-      // Redirect to the user page
-      header("Location: user.php");
-      exit;
-    } else {
-      echo "Incorrect password!";
-      return;
-    }
-  }
-
-  echo "Email or phone number does not exist!";
 }
 
-// Perform login if data is submitted from the form
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $username = $_POST['Email'];
-  $password = $_POST['password'];
-
-  user_login($username, $password);
-}
-
+// Call the login function
+login();
 ?>
 
 <!DOCTYPE html>
@@ -76,22 +75,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/reset.css">
-    <link rel="stylesheet" href="../css/app.css">
+    <!-- <link rel="stylesheet" href="css/reset.css">-->
+    <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
 
     <title>Login Page</title>
 </head>
 <body>
     <div id="wrapper">
-        <form action="" id="form-login">
+        <form action="login.php" id="form-login" method="POST">
             <h1 class="form-heading">Sign In</h1>
             <div class="form-group">
                 <i class="far fa-user"></i>
-                <input type="text" name="Email" id="Email" class="form-input" placeholder="Email" required /> 
+                <input type="email" name="email" class="form-input" placeholder="Email" required /> 
             </div>
             <div class="form-group">
-                <input type="password" id="password" name="password" class="form-input" placeholder="Password" required />
+                <input type="password" name="password" class="form-input" placeholder="Password" required />
                 <div id="eye">
                     <i class="far fa-eye"></i>
                 </div>
@@ -99,17 +98,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             <div class="btn btn-primary">
               <button type="submit" class="btn btn-primary form-submit" type="submit" name="login">Log In</button>
             </div>
-            <!-- <input type="submit" value="Log In" class="form-submit"> -->
-            <div class="forgot-password">
-                <a href="#">Forgot password?</a>
-            </div>
-            <div class="register-link">
-            Don't have an account? <a href="#">Register here</a>
-            </div>
         </form>
         
     </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-<script src="../js/app.js"></script>
+<script src="js/app.js"></script>
 </html>
