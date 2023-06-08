9e636f17c975df73b31cc7e7e578e71e10e6f8b2
AarJul <barongdong3@gmail.com>
2023-06-08 10:28:01 +0900

    modified:   Register.html 	new file:   StoreInvnt.html 	new file:   css/storeInvnt.css 	new file:   js/inventory.js

---
+++ js/inventory.js
@@ -0,0 +1,45 @@
+function sortTable(columnIndex) {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("inventoryTable");
+    switching = true;
+  
+    // Set the sorting direction to ascending
+    var direction = "asc";
+  
+    while (switching) {
+      switching = false;
+      rows = table.rows;
+  
+      for (i = 1; i < rows.length - 1; i++) {
+        shouldSwitch = false;
+        x = rows[i].getElementsByTagName("TD")[columnIndex];
+        y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
+  
+        // Compare the cell values for sorting
+        if (direction === "asc") {
+          if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        } else if (direction === "desc") {
+          if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+            shouldSwitch = true;
+            break;
+          }
+        }
+      }
+  
+      if (shouldSwitch) {
+        rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+        switching = true;
+      }
+  
+      // Toggle the sorting direction
+      if (direction === "asc") {
+        direction = "desc";
+      } else {
+        direction = "asc";
+      }
+    }
+  }
+  
