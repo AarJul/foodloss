1f07557d7cd83b27331c6c416b680e0c4e4db7ce
AarJul <barongdong3@gmail.com>
2023-06-08 10:42:04 +0900

    modified:   Register.html 	modified:   StoreInvnt.html 	modified:   js/inventory.js

--- js/inventory.js
+++ js/inventory.js
@@ -1,20 +1,21 @@
-function sortTable(columnIndex) {
+{
+  function sortTable(columnIndex) {
     var table, rows, switching, i, x, y, shouldSwitch;
     table = document.getElementById("inventoryTable");
     switching = true;
-  
+
     // Set the sorting direction to ascending
     var direction = "asc";
-  
+
     while (switching) {
       switching = false;
       rows = table.rows;
-  
+
       for (i = 1; i < rows.length - 1; i++) {
         shouldSwitch = false;
         x = rows[i].getElementsByTagName("TD")[columnIndex];
         y = rows[i + 1].getElementsByTagName("TD")[columnIndex];
-  
+
         // Compare the cell values for sorting
         if (direction === "asc") {
           if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
@@ -28,12 +29,12 @@ function sortTable(columnIndex) {
           }
         }
       }
-  
+
       if (shouldSwitch) {
         rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
         switching = true;
       }
-  
+
       // Toggle the sorting direction
       if (direction === "asc") {
         direction = "desc";
@@ -42,4 +43,4 @@ function sortTable(columnIndex) {
       }
     }
   }
-  
+}
