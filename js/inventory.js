6a3523ec48ff66a4d698bf9cb5dbd05fbd5fa4bc
AarJul <barongdong3@gmail.com>
2023-06-12 11:35:45 +0900

    modified:   js/inventory.js 	modified:   w_Store_Inventory/StoreInvnt.html

--- js/inventory.js
+++ js/inventory.js
@@ -22,53 +22,79 @@
   }
 
   var inventoryData = [
-    { product: "Product 1", quantity: 10, price: "$20" },
-    { product: "Product 2", quantity: 5, price: "$15" }
+    {
+      storeId: "Store 1",
+      disposalInfo: "Info 1",
+      item: "Item 1",
+      quantity: 10,
+      date: "2023-06-12",
+      status: "Active",
+    },
+    {
+      storeId: "Store 2",
+      disposalInfo: "Info 2",
+      item: "Item 2",
+      quantity: 5,
+      date: "2023-06-11",
+      status: "Inactive",
+    },
     // Add more inventory data as needed
   ];
-  
+
   // Generate the table rows dynamically
   function generateTableRows() {
     var tbody = document.getElementById("inventoryBody");
-  
+
     // Clear the existing table body
     tbody.innerHTML = "";
-  
+
     // Generate a table row for each item in the inventory
-    inventoryData.forEach(function(item) {
+    inventoryData.forEach(function (item) {
       var row = document.createElement("tr");
-  
+
       // Create table cells for each item property
-      var productCell = document.createElement("td");
-      productCell.textContent = item.product;
-      row.appendChild(productCell);
-  
+      var storeIdCell = document.createElement("td");
+      storeIdCell.textContent = item.storeId;
+      row.appendChild(storeIdCell);
+
+      var disposalInfoCell = document.createElement("td");
+      disposalInfoCell.textContent = item.disposalInfo;
+      row.appendChild(disposalInfoCell);
+
+      var itemCell = document.createElement("td");
+      itemCell.textContent = item.item;
+      row.appendChild(itemCell);
+
       var quantityCell = document.createElement("td");
       quantityCell.textContent = item.quantity;
       row.appendChild(quantityCell);
-  
-      var priceCell = document.createElement("td");
-      priceCell.textContent = item.price;
-      row.appendChild(priceCell);
-  
+
+      var dateCell = document.createElement("td");
+      dateCell.textContent = item.date;
+      row.appendChild(dateCell);
+
+      var statusCell = document.createElement("td");
+      statusCell.textContent = item.status;
+      row.appendChild(statusCell);
+
       var actionCell = document.createElement("td");
       var deleteButton = document.createElement("button");
       deleteButton.classList.add("btn", "btn-danger", "btn-sm");
       deleteButton.textContent = "Delete";
-      deleteButton.addEventListener("click", function() {
+      deleteButton.addEventListener("click", function () {
         deleteItem(row);
       });
       actionCell.appendChild(deleteButton);
       row.appendChild(actionCell);
-  
+
       // Append the row to the table body
       tbody.appendChild(row);
     });
   }
-  
+
   // Call the function to generate table rows
   generateTableRows();
-  
+
   // Delete an item from the table
   function deleteItem(row) {
     row.remove();
