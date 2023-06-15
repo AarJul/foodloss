d4e76aa9312292351b2778ec95e5b14c591830a5
QuanNguyen8701 <quanrengar@gmail.com>
2023-06-15 09:28:31 +0900

    ok

--- w_store_page/disposal_registration.php
+++ w_store_page/disposal_registration.php
@@ -1,7 +1,5 @@
 <?php
-//未完成
-
-// データベースの情報　
+// データベースの情報
 $servername = "localhost";
 $username = "dbuser";
 $password = "ecc";
@@ -9,26 +7,173 @@ $dbname = "food";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-//接続のチェック
+// 接続のチェック
 if ($conn->connect_error) {
     die("アクセス失敗: " . $conn->connect_error);
 }
 
-$sql;
-//フォームから送信されたデータを取得   
-$storeID = $_POST['storeId'];
-$iteml = $_POST['item'];
-$qty = $_POST['qty'];
-$date = $_POST['date'];
+// フォームから送信されたデータを取得
+$store_id = $_POST['store_id'];
+$name = isset($_POST['name']) ? $_POST['name'] : '';
+$quantity = isset($_POST['quantity']) ? $_POST['quantity'] : '';
+$date = isset($_POST['date']) ? $_POST['date'] : '';
 
-// Chuẩn bị truy vấn SQL để chèn dữ liệu vào bảng
-$sql = "INSERT INTO disposal (storeID, item, qty, date) VALUES ('$storeID', '$item', '$qty', '$date')";
+// $name、$quantity、$date がデータを含んでいる場合のみ処理を実行
+if (!empty($name) && !empty($quantity) && !empty($date)) {
+    // データを挿入するためのSQLクエリを準備
+    $sql = "INSERT INTO disposal (store_ID, item, qty, date) VALUES ('$store_id', '$name', '$quantity', '$date')";
 
-// Thực thi truy vấn và kiểm tra kết quả
-if ($conn->query($sql) === TRUE) {
-    echo "Dữ liệu đã được chèn thành công vào bảng.";
-} else {
-    echo "Lỗi: " . $sql . "<br>" . $conn->error;
+    // クエリを実行して結果をチェック
+    if ($conn->query($sql) === TRUE) {
+        echo "データがテーブルに正常に挿入されました。";
+    } else {
+        echo "エラー: " . $sql . "<br>" . $conn->error;
+    }
 }
 
+$conn->close();
 ?>
+
+
+
+<html lang="jp">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Page</title>
+</head>
+
+<body>
+    <h1>廃棄登録</h1>
+ <!-- Các phần còn lại của trang disposal_registration.html -->
+    <!-- <form method="post" action="disposal_registration.php"> -->
+        <p><?php echo $store_id; ?></p>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br><br>
+
+        <label for="quantity">Quantity:</label>
+        <input type="text" id="quantity" name="quantity" required><br><br>
+        <script>
+          const quantityInput = document.getElementById('quantity');
+        
+          quantityInput.addEventListener('input', function(event) {
+            const input = event.target.value;
+            const pattern = /^[0-9]+$/;
+        
+            if (!pattern.test(input)) {
+              event.target.value = input.replace(/[^0-9]/g, '');
+            }
+          });
+        </script>
+        
+        <label for="date">Date:</label>
+        <input type="date" id="date" name="date" required><br><br>
+
+        <!-- <input type="button" value="Submit" onclick="submitForm()"> -->
+        <button onclick="showPopup()">登録</button>
+
+        <!-- Popup -->
+        <div id="popup" class="popup">
+          <div class="popup-content">
+            <p>Bạn muốn tiếp tục hay quay lại?</p>
+            <div class="button-container">
+              <button onclick="continueAction()">続ける</button>
+              <button onclick="goBackAction()">戻る</button>
+            </div>
+          </div>
+        </div>
+        
+        <!-- CSS -->
+        <style>
+          .popup {
+            display: none;
+            position: fixed;
+            z-index: 9999;
+            left: 0;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow: auto;
+            background-color: rgba(0, 0, 0, 0.4);
+          }
+        
+          .popup-content {
+            background-color: #fefefe;
+            margin: 15% auto;
+            padding: 20px;
+            border: 1px solid #888;
+            width: 300px;
+            text-align: center;
+          }
+        
+          .button-container {
+            margin-top: 20px;
+          }
+        </style>
+        
+        <!-- JavaScript -->
+        <script>
+          function showPopup() {
+            var nameInput = document.getElementById("name");
+            var quantityInput = document.getElementById("quantity");
+            var dateInput = document.getElementById("date");
+            
+            if (nameInput.value !== "" && quantityInput.value !== "" && dateInput.value !== "") {
+              var popup = document.getElementById("popup");
+              popup.style.display = "block";
+            } else {
+              alert("Vui lòng nhập đầy đủ thông tin.");
+            }
+          }
+        
+          function continueAction() {
+            var nameInput = document.getElementById("name");
+            var quantityInput = document.getElementById("quantity");
+            var dateInput = document.getElementById("date");
+
+            var data = {
+              name: nameInput.value,
+              quantity: quantityInput.value,
+              date: dateInput.value
+            };
+
+            fetch("disposald_registration.php", {
+              method: "POST",
+              body: JSON.stringify(data),
+              headers: {
+                "Content-Type": "application/json"
+              }
+            })
+            .then(function(response) {
+              if (response.ok) {
+                return response.text();
+              } else {
+                throw new Error("Đã xảy ra lỗi khi gửi dữ liệu.");
+              }
+            })
+            .then(function(data) {
+              alert(data); // Hiển thị thông báo từ PHP trong hộp thoại
+              closePopup();
+            })
+            .catch(function(error) {
+              alert("Đã xảy ra lỗi khi gửi dữ liệu: " + error);
+            });
+          }
+
+        
+          function goBackAction() {
+            // closePopup();
+            window.location.href = "trang_web_khac.html"; // Thay đổi đường dẫn "trang_web_khac.html" thành trang web muốn chuyển hướng đến
+          }
+        
+          function closePopup() {
+            var popup = document.getElementById("popup");
+            popup.style.display = "none";
+          }
+        </script>
+        
+    <!-- </form> -->
+</body>
+
+</html>
