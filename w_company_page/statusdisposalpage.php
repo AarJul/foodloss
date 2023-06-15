fc335ee5b720de29a72c9675347a814e9d21f906
QuanNguyen8701 <quanrengar@gmail.com>
2023-06-15 10:02:11 +0900

    ok

--- w_company_page/statusdisposalpage.php
+++ w_company_page/statusdisposalpage.php
@@ -46,7 +46,7 @@ $conn->close();
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Home</title>
+    <title>Homeádfdsafds</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link
