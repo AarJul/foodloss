d86680279181fae5a7d39abaaa5dc1013e3ba486
mintie98 <thang3dinhat@gmail.com>
2023-06-22 12:02:10 +0900

    a

--- user.php
+++ user.php
@@ -2,10 +2,10 @@
 session_start();
 require_once dirname(__FILE__) . '/function/db_connection.php';
 
- 
+
 $conn = connection();
 $message = '';
- 
+
 // Kiểm tra xem người dùng đã đăng nhập hay chưa
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
@@ -21,7 +21,7 @@ $userStmt = $conn->prepare($userQuery);
 $userStmt->bindParam(1, $user_id);
 $userStmt->execute();
 $userResult = $userStmt->fetch(PDO::FETCH_ASSOC);
- 
+
 // Truy vấn thông tin store và disposal
 $storeQuery = "SELECT s.STORE_ID, s.STORE_NAME, d.ITEM, d.QTY, d.DATE
                 FROM store s
@@ -30,7 +30,7 @@ $storeStmt = $conn->prepare($storeQuery);
 $storeStmt->execute();
 $storeResult = $storeStmt->fetchAll(PDO::FETCH_ASSOC);
 
- 
+
 if ($userResult) {
     $user_id = $userResult['USER_ID'];
     $user_name = $userResult['USER_NAME'];
@@ -40,14 +40,14 @@ if ($userResult) {
     header("Location: error.php");
     exit();
 }
- 
+
 $conn = null;
 ?>
 
- 
+
 <!DOCTYPE html>
 <html lang="ja">
- 
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -55,7 +55,7 @@ $conn = null;
     <link rel="stylesheet" type="text/css" href="css/user.css">
 </head>
 
- 
+
 <body>
     <div class="container">
         <div class="top-right-section">
@@ -110,7 +110,7 @@ $conn = null;
                                 <td colspan="3">No disposal data</td>
                             </tr>
                             <?php
-               }
+                }
             }
             ?>
                 </table>
@@ -124,4 +124,5 @@ $conn = null;
         }
     </script>
 </body>
+
 </html>
