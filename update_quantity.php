32d8cca20d7476d5e8d537b994f7569a78d91427
mintie98 <thang3dinhat@gmail.com>
2023-06-23 12:16:01 +0900

    error

---
+++ update_quantity.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+require_once dirname(__FILE__) . '/function/db_connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Kiểm tra xem người dùng đã đăng nhập hay chưa
+    if (!isset($_SESSION['user_id'])) {
+        header("Location: login.php");
+        exit();
+    }
+
+    // Lấy thông tin từ yêu cầu AJAX
+    $storeId = $_POST['store_id'];
+    $quantity = $_POST['quantity'];
+
+    // Cập nhật số lượng trong cơ sở dữ liệu
+    $conn = connection();
+    $updateQuery = "UPDATE disposal SET QTY = QTY - ? WHERE DISPOSAL_ID = ?";
+    $updateStmt = $conn->prepare($updateQuery);
+    $updateStmt->bindParam(1, $quantity);
+    $updateStmt->bindParam(2, $storeId);
+    $updateStmt->execute();
+    $conn = null;
+
+    // Trả về phản hồi thành công
+    echo "Success";
+}
+?>
--- user.php
+++ user.php
@@ -2,7 +2,6 @@
 session_start();
 require_once dirname(__FILE__) . '/function/db_connection.php';
 
-
 $conn = connection();
 $message = '';
 
@@ -12,7 +11,6 @@ if (!isset($_SESSION['user_id'])) {
     exit();
 }
 $store_id = isset($_SESSION['store_id']) ? $_SESSION['store_id'] : null;
-
 
 // Truy vấn thông tin user dựa trên user_id
 $user_id = $_SESSION['user_id'];
@@ -29,7 +27,6 @@ $storeQuery = "SELECT s.STORE_ID, s.STORE_NAME, d.ITEM, d.QTY, d.DATE
 $storeStmt = $conn->prepare($storeQuery);
 $storeStmt->execute();
 $storeResult = $storeStmt->fetchAll(PDO::FETCH_ASSOC);
-
 
 if ($userResult) {
     $user_id = $userResult['USER_ID'];
@@ -53,19 +50,42 @@ $conn = null;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Quản lý hàng tồn kho</title>
     <link rel="stylesheet" type="text/css" href="css/user.css">
-</head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/footer.css"/>
+    <link rel="stylesheet" href="css/navbar.css"/>
+    <link rel="stylesheet" href="css/storeInvnt.css"/>
 
+</head>
 
 <body>
     <div class="container">
+    <nav class="navbar navbar-inverse fixed-top">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="./w_aboutUs/about.html">OpenSeaS</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">ホーム</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">ストア用<span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">ストアフロント</a></li>
+                        <li><a href="./w_disposal_page/registerDisposal.html">廃棄登録</a></li>
+                        <li><a href="./w_store_page/storeInfo.html">ストア情報</a></li>
+                    </ul>
+                </li>
+                <li><a href="./w_disposal_page/deliveryDisposal">廃棄情報</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="../w_Account_Register/Register.html"><span class="glyphicon glyphicon-user"><?php echo $user_name; ?></span>
+                        </a>
+                </li>
+                <li id="user">
+                    <a href="../login.php"><span class="glyphicon glyphicon-log-in"></span> Logout</a>
+                </li>
+            </ul>
+        </nav>
         <div class="top-right-section">
-            <h2>User ID: <span id="userid">
-                    <?php echo $user_id; ?>
-                </span></h2>
-            <h2>Tên: <span id="username">
-                    <?php echo $user_name; ?>
-                </span></h2>
-            <button onclick="logout()">Logout</button>
             <a href="confirm.php"><button>Xác nhận đơn hàng</button></a>
         </div>
         <div class="center-section">
@@ -84,15 +104,9 @@ $conn = null;
                             </h3>
                             <thead>
                                 <tr>
-                                    <th onclick="sortTable(0)">
-                                        商品名 <span class="glyphicon glyphicon-sort"></span>
-                                    </th>
-                                    <th onclick="sortTable(1)">
-                                        期限 <span class="glyphicon glyphicon-sort"></span>
-                                    </th>
-                                    <th onclick="sortTable(2)">
-                                        個数 <span class="glyphicon glyphicon-sort"></span>
-                                    </th>
+                                    <th onclick="sortTable(0)">商品名 <span class="glyphicon glyphicon-sort"></span></th>
+                                    <th onclick="sortTable(1)">期限 <span class="glyphicon glyphicon-sort"></span></th>
+                                    <th onclick="sortTable(2)">個数 <span class="glyphicon glyphicon-sort"></span></th>
                                 </tr>
                             </thead>
                             <tbody id="inventoryBody">
@@ -105,25 +119,17 @@ $conn = null;
                     // Hiển thị thông tin disposal của store hiện tại
                     ?>
                             <tr>
-                                <td>
-                                    <?php echo $store['ITEM']; ?>
-                                </td>
-                                <td>
-                                    <?php echo $store['QTY']; ?>
-                                </td>
-                                <td>
-                                    <?php echo $store['DATE']; ?>
-                                </td>
-                                <td>
-                                    <button>要求</button>
-                                </td>
+                                <td><?php echo $store['ITEM']; ?></td>
+                                <td><?php echo $store['DATE']; ?></td>
+                                <td id="qty_<?php echo $store['STORE_ID']; ?>"><?php echo $store['QTY']; ?></td>
+                                <td><button class="request-button" data-storeId="<?php echo $store['STORE_ID']; ?>">要求</button></td>
                             </tr>
                             <?php
                 } else {
                     // Hiển thị thông báo khi không có disposal data
                     ?>
                             <tr>
-                                <td colspan="3">No disposal data</td>
+                                <td colspan="4">No disposal data</td>
                             </tr>
                             <?php
                 }
@@ -150,12 +156,19 @@ $conn = null;
             </div>
         </footer>
     </div>
-    <script src="script.js"></script>
-    <script>
-        function logout() {
-            window.location.href = "login.php";
-        }
-    </script>
+    <!-- Modal -->
+    <div id="modal-container" class="modal">
+        <div class="modal-content">
+            <span class="close" onclick="closeModal()">&times;</span>
+            <h2>Yêu cầu số lượng</h2>
+            <input type="text" id="quantityInput" placeholder="Nhập số lượng">
+            <button id="submitRequestBtn" onclick="submitRequest()">Yêu cầu</button>
+        </div>
+    </div>
+
+
+    <script src="js/bootstrap.js"></script>
+    <script src="js/userScript.js"></script>
 </body>
 
 </html>
